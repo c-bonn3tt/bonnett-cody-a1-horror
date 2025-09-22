03441024ef40bc6a2b9e703e1e5730415ffbaa03
c-bonn3tt <cody.bonnett@mohawkcollege.ca>
2025-09-22 17:30:15 -0400

    Added choice between 1 and 4

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿//Asking Name
+Console.WriteLine("What is your name?");
+
+string Name = Console.ReadLine();
+Console.WriteLine($"Greetings {Name}, Welcome to my game!");
+
+//Picking a number betweeen 1 and 4
+Console.WriteLine("Pick a number between 1 and 4");
+string chosenNumber = Console.ReadLine();
+int testValue = int.Parse(chosenNumber);
+if (testValue < 1)
+{
+    Console.WriteLine("The number you chose is too low, please play again.");
+testValue = int.Parse(chosenNumber);
+
+}
+else if (testValue > 4)
+{
+    Console.WriteLine("The number you chose is too high, please play again.");
+    int newChoice = int.Parse(chosenNumber);
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # bonnett-cody-a1-horror
 This is a short horror game for my Interactive Terminal Project
+
+Numbers must be intergers between 1 and 4.
