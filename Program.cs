54255641b900172a3aacfadfae9050a285a5f9e5
c-bonn3tt <cody.bonnett@mohawkcollege.ca>
2025-09-22 22:58:25 -0400

    some organizing and added more if statements

--- Program.cs
+++ Program.cs
@@ -1,6 +1,13 @@
 ﻿//Asking Name
 using System.ComponentModel.Design;
 
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        
+
+        bool winCondion = false;
 Console.WriteLine("What is your name?");
 
 string Name = Console.ReadLine();
@@ -8,6 +15,7 @@ Console.WriteLine($"Greetings {Name}, Welcome to my game!");
 
 //Picking a number betweeen 1 and 4
 Console.WriteLine("Pick a number between 1 and 4");
+        
 string chosenNumber = Console.ReadLine();
 int testValue = int.Parse(chosenNumber);
 if (testValue < 1)
@@ -23,26 +31,36 @@ else if (testValue > 4)
 }
 
 Console.WriteLine("Thank you for choosing a number. \nYou wake up . . . insert morning routine");
-Console.WriteLine("question 1 (Yes/No)");
+
+        Console.WriteLine("You leave the house, as you are getting in your car, you can't remember if you shut off the stove. Go back and check? (Yes/No)");
 //Trying to make Yes/No as valid boolion
 {
-    string firstResponse = Console.ReadLine();
+            string userResponse = Console.ReadLine().ToLower();
 
-    bool procceed;
+            bool procceed = false;
 
-    if (firstResponse == "Yes")
+            if (userResponse == "yes")
     {  
         procceed = true; 
     }
     
-    else if (firstResponse == "No")
+            else if (userResponse == "no")
     {  
         procceed = false; 
     }
+            
     else
     {
         Console.WriteLine("Please only type 'Yes' or 'No'. ");
     }
-
-
+            if ((procceed == true) && (testValue == 2)) ;
+            {
+                winCondion = true;
+            }
+             else if ((procceed == true) && (testValue != 2)) ;
+            { 
+                winCondion = false; 
+        }
 }
+                }
+                }
