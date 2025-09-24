c8217bc286a720fc7b9240d9be27f6efd5196589
c-bonn3tt <cody.bonnett@mohawkcollege.ca>
2025-09-24 14:13:39 -0400

    Finished simple mechanics and added story

--- Program.cs
+++ Program.cs
@@ -1,4 +1,3 @@
-﻿using System.ComponentModel.Design;
 
 internal class Program
 {
@@ -11,11 +10,21 @@ internal class Program
         Console.WriteLine("What is your name?");
 
         string Name = Console.ReadLine();
-        Console.WriteLine($"Greetings {Name}, Welcome to my game!");
+        Console.WriteLine($"Greetings {Name}, \nYou are driving to work when you start questioning to yourself what you did this morning. \nYou were so tired you couldn't remember.\nTo you, it feels like you suddenly appeared in your car. \nSuddenly, you feel like you're forgetting something and must recall what you did otherwise it can have disastrous consequences.");
+        Console.WriteLine();
         {
             //Picking a number betweeen 1 and 4
-            Console.WriteLine("Pick a number between 1 and 4");
+            Console.WriteLine("Pick one of the 4 scenarios that you can remember from some point in your life:");
 
+            Console.WriteLine("1. You wake up to sounds of the city, cars honking and random people shouting. \nYou quickly chow down your breakfast of bacon and eggs before going out and getting a little exercise with your pup. \nAfter returning you make sure to take your meds, grab your keys and head out the door.\n");
+
+            Console.WriteLine("2. Its a typical morning, you are woken up by your dog needing to go out. While the dog is out, \nyou eat a sandwich and take your meds. \nAfter letting the dog in, you have a shower and get ready. \nChecking the time, you realize you are running late and you run out the door.\n");
+
+            Console.WriteLine("3. After frequent late nights, you tend to feel sluggish and lazy. \nYou get up, eat cereal, feed your dog, and shower before \ntaking your meds and heading out the door.\n");
+
+            Console.WriteLine("4. You suddenly woke up in your car and can't remember what you did this morning.\n");
+
+            Console.WriteLine("Pick by typing the corresponding number below (e.g '1'):");
             string chosenNumber = Console.ReadLine();
             int testValue = int.Parse(chosenNumber);
             if (testValue < 1)
@@ -30,9 +39,9 @@ internal class Program
                 int newChoice = int.Parse(chosenNumber);
             }
 
-            Console.WriteLine("Thank you for choosing a number. \nYou wake up . . . insert morning routine");
+            Console.WriteLine($"You have chosen:{chosenNumber}\n");
 
-            Console.WriteLine("You leave the house, as you are getting in your car, you can't remember if you shut off the stove. Go back and check? (Yes/No)");
+            Console.WriteLine("The door is locked. (Yes/No)");
             //Trying to make Yes/No as valid boolion
             {
                 string userResponse = Console.ReadLine().ToLower();
@@ -70,6 +79,7 @@ internal class Program
                 }
                 Console.WriteLine($"{testValue}, {winCondion}, {loseCondition}"); //Just observing the values throughout the questions
             }
+
                 Console.WriteLine("You leave the house, as you are getting in your car, you can't remember if you shut off the stove. Go back and check? (Yes/No)");
             //Trying to make Yes/No as valid boolion
             {
@@ -104,10 +114,11 @@ internal class Program
                 {
                   //  winCondion = false;
                 }
-                Console.WriteLine($"{testValue}, {winCondion}, {loseCondition}"); //Just observing the values throughout the questions
+                // Console.WriteLine($"{testValue}, {winCondion}, {loseCondition}"); //bool values
             }
-                Console.WriteLine("You leave the house, as you are getting in your car, you can't remember if you shut off the stove. Go back and check? (Yes/No)");
-            //Trying to make Yes/No as valid boolion
+
+                Console.WriteLine("The dog has gone out. (Yes/No)");
+
             {
                 string userResponse = Console.ReadLine().ToLower();
                 bool procceed = false;
@@ -140,9 +151,10 @@ internal class Program
                 {
                   //  winCondion = false;
                 }
-                Console.WriteLine($"{testValue}, {winCondion}, {loseCondition}"); //Just observing the values throughout the questions
+                Console.WriteLine($"{testValue}, {winCondion}, {loseCondition}"); //Bool values
             }
-                Console.WriteLine("You leave the house, as you are getting in your car, you can't remember if you shut off the stove. Go back and check? (Yes/No)");
+
+                Console.WriteLine("Meds have been taken. (Yes/No)");
                 //Trying to make Yes/No as valid boolion
                 {
                     string userResponse = Console.ReadLine().ToLower();
@@ -176,10 +188,10 @@ internal class Program
                     {
                        // winCondion = false;
                     }
-                Console.WriteLine($"{ testValue}, { winCondion}, { loseCondition}"); //Just observing the values throughout the questions
-                if (winCondion == true && loseCondition == false) { Console.WriteLine($"Congratulations!{Name} You won! WINNER GANON!"); }
+                    //Console.WriteLine($"{ testValue}, { winCondion}, { loseCondition}"); //bool value check
+                    if (winCondion == true && loseCondition == false) { Console.WriteLine($"Congratulations {Name}! You figured it out!"); }
                 else {
-                    Console.WriteLine("Game Over, try again!");
+                    Console.WriteLine("Good try! Please play again!");
                             }
 
                 }
